a076b8d46a1c0de614d34daafff713d8383b2e5c
Rui-Xian-45 <zxc0930188136@gmail.com>
2026-07-02 22:47:12 +0800

    Update embeds.py

--- embeds.py
+++ embeds.py
@@ -1,13 +1,23 @@
 import discord
 
-
-def build_embed(queue):
+def build_embed(queue, guild: discord.Guild = None):
 
     members = queue.data["members"]
     i = queue.data["current_index"]
 
-    current = members[i:i+3]
-    next_group = members[i+3:i+6]
+    current_ids = members[i:i+3]
+    next_ids = members[i+3:i+6]
+
+    # ⭐ 轉名字
+    def get_name(uid):
+        if guild:
+            member = guild.get_member(uid)
+            if member:
+                return member.display_name
+        return str(uid)
+
+    current = [get_name(uid) for uid in current_ids]
+    next_group = [get_name(uid) for uid in next_ids]
 
     return discord.Embed(
         title="⚔ RO 副本排隊系統",
